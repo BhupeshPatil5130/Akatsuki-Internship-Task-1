d3a862d01e1ab4ed53f34b6dcf4fe0de6568fd45
Bhupesh Patil <160297608+BhupeshPatil5130@users.noreply.github.com>
2024-09-16 19:29:28 +0530

    Edited the app.jsx file and add the component in it

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 
 import './App.css'
 
+//import Blog from './components/Content'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Main from './components/Main'
+
+
+
+
+
 function App() {
-
-
+  
   return (
-    <>
-     
-    </>
+  <>
+      <div>
+           
+         
+          
+            <Header/>
+            <Hero/>
+            <Main/>
+            <Footer/>
+      </div>
+  </>
+      
   )
 }
 
