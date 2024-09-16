81e5f9f78e42818fae0581cc973a856451900a57
Bhupesh Patil <160297608+BhupeshPatil5130@users.noreply.github.com>
2024-09-16 19:31:23 +0530

    Added the data of the blog in a blogPost.js file

---
+++ src/components/blogPosts.js
@@ -0,0 +1,125 @@
+
+import blog1 from '../assets/images/blog-1.png';
+import blog2 from '../assets/images/blog-2.png';
+import blog3 from '../assets/images/blog-3.png';
+import blog4 from '../assets/images/blog-4.png';
+import blog5 from '../assets/images/blog-5.png';
+import blog6 from '../assets/images/blog-6.png';
+import blog7 from '../assets/images/blog-7.png';
+import blog8 from '../assets/images/blog-8.png';
+import blog9 from '../assets/images/blog-9.png';
+import blog10 from '../assets/images/blog-10.png';
+
+
+export const blogPosts = [
+  {
+    id: 1,
+    image: blog1,
+    alt: 'Building microservices with Dropwizard, MongoDB & Docker',
+    topic: 'Database',
+    title: 'Building microservices with Dropwizard, MongoDB & Docker',
+    excerpt: 'This NoSQL database oriented to documents (by documents like JSON) combines some of the features from relational databases...',
+    author: 'Julia Walker',
+    date: '2022-01-17',
+    time: '3 min',
+  },
+  {
+    id: 2,
+    image: blog2,
+    alt: 'Fast web page loading on a $20 feature phone',
+    topic: 'Web Performance',
+    title: 'Fast web page loading on a $20 feature phone',
+    excerpt: 'Feature phones are affordable (under $20-25), low-end devices enabling 100s of millions of users in developing countries to leverage the web...',
+    author: 'Julia Walker',
+    date: '2021-12-10',
+    time: '2 min',
+  },
+  {
+    id: 3,
+    image: blog3,
+    alt: 'Accessibility Tips for Web Developers',
+    topic: 'Accessibility',
+    title: 'Accessibility Tips for Web Developers',
+    excerpt: 'It\'s awesome to build sites that are inclusive and accessible to everyone. There are at least six key areas of disability we can optimize for...',
+    author: 'Julia Walker',
+    date: '2021-11-28',
+    time: '4 min',
+  },
+  {
+    id: 4,
+    image: blog4,
+    alt: 'Dynamically Securing Databases using Hashicorp Vault',
+    topic: 'Database',
+    title: 'Dynamically Securing Databases using Hashicorp Vault',
+    excerpt: 'Nowadays, it\'s hard to profoundly talk about security in the IT industry, since it has to be considered on so many different levels...',
+    author: 'Julia Walker',
+    date: '2021-11-20',
+    time: '4 min',
+  },
+  {
+    id: 5,
+    image: blog5,
+    alt: 'Adaptive Loading - Improving Web Performance on low-end devices',
+    topic: 'Web Performance',
+    title: 'Adaptive Loading - Improving Web Performance on low-end devices',
+    excerpt: 'Adaptive Loading: Do not just respond based on screen size, adapt based on actual device hardware. Any user can have a slow experience...',
+    author: 'Julia Walker',
+    date: '2021-11-10',
+    time: '3 min',
+  },
+  {
+    id: 6,
+    image: blog6,
+    alt: 'Don\'t Develop Just for Yourself - A Developer\'s Checklist to Accessibility',
+    topic: 'Accessibility',
+    title: 'Don\'t Develop Just for Yourself - A Developer\'s Checklist to Accessibility',
+    excerpt: 'We, as developers, tend to develop sites unconsciously for people like ourselves. If we don\'t actively pay attention, the sites are often accessible only for certain types of people...',
+    author: 'Julia Walker',
+    date: '2021-10-25',
+    time: '7 min',
+  },
+  {
+    id: 7,
+    image: blog7,
+    alt: 'Building a Restful CRUD API with Node JS, Express, and MongoDB',
+    topic: 'Database',
+    title: 'Building a Restful CRUD API with Node JS, Express, and MongoDB',
+    excerpt: 'Application Programming Interface is the abbreviation for API. An API is a software interface that enables two apps to communicate with one another...',
+    author: 'Julia Walker',
+    date: '2021-10-15',
+    time: '5 min',
+  },
+  {
+    id: 8,
+    image: blog8,
+    alt: 'Monitoring Performance with the PageSpeed Insights API',
+    topic: 'Web Performance',
+    title: 'Monitoring Performance with the PageSpeed Insights API',
+    excerpt: 'The PageSpeed Insights API provides free access to performance monitoring for web pages and returns data with suggestions for how to improve...',
+    author: 'Julia Walker',
+    date: '2021-10-03',
+    time: '5 min',
+  },
+  {
+    id: 9,
+    image: blog9,
+    alt: 'The best web accessibility tools for developers in 2021',
+    topic: 'Accessibility',
+    title: 'The best web accessibility tools for developers in 2021',
+    excerpt: 'The quality of the tools you use defines the speed with which you can diagnose and resolve problems. Each year the landscape changes dramatically in web technologies...',
+    author: 'Julia Walker',
+    date: '2021-09-13',
+    time: '7 min',
+  },
+  {
+    id: 10,
+    image: blog10,
+    alt: 'How to connect a React frontend with a NodeJS/Express backend',
+    topic: 'Database',
+    title: 'How to connect a React frontend with a NodeJS/Express backend',
+    excerpt: 'The MERN (MongoDB, Express, React, NodeJS) stack is very popular for making full stack applications, utilizing Javascript for both the backend and frontend as well as a document-oriented or non relational database (MongoDB)...',
+    author: 'Julia Walker',
+    date: '2021-09-21',
+    time: '4 min',
+  },
+];
